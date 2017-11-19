7d8751e74b63b0e84a196d8c9d5bce8d79782b10
venantvr <r.venant.valery@gmail.com>
2017-11-19 20:42:08 +0000

    Tests to console app...

---
+++ Neo4jClient.Extension/test/Neo4jClient.Extension.Test.ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Neo4jClient.Extension.Test.Cypher;
+
+namespace Neo4jClient.Extension.Test.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cypherExtensionTests = new CypherExtensionTests();
+            
+            cypherExtensionTests.EntityLabelWithoutAttrTest();
+            cypherExtensionTests.EntityLabelWithTest();
+            cypherExtensionTests.ToCypherStringMergeTest();
+            cypherExtensionTests.MatchAllTest();
+            cypherExtensionTests.MergeEntityOverrideMergeTest();          
+        }
+    }
+}
